34422f97ce23fd8bc2ad38aafac34cdd4f2d7d89
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-03 21:55:57 +0900

    how to input value at input field

--- src/views/Todos/AddTodo.js
+++ src/views/Todos/AddTodo.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
 class AddTodo extends Component {
+    state = {
+        title: ''
+    }
     render() {
+        let { title } = this.state;
         return (
             <div className="add-todo">
-                <input type="text" />
+                <input type="text" value={title} />
                 <button type="button" className='add'>Add</button>
             </div>
         );
