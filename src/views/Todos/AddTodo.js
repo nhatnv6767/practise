508bdf69a99a1f6e452b97524b08abe81dfb5106
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-03 22:02:12 +0900

    add todo and use child to call parent

--- src/views/Todos/AddTodo.js
+++ src/views/Todos/AddTodo.js
@@ -11,6 +11,17 @@ class AddTodo extends Component {
         })
     }
 
+    handleAddTodo = () => {
+        let todo = {
+            id: Math.floor(Math.random() * 1000000),
+            title: this.state.title
+        }
+        this.props.addNewTodo(todo)
+        this.setState({
+            title: ''
+        })
+    }
+
     render() {
         let { title } = this.state;
         return (
@@ -18,7 +29,11 @@ class AddTodo extends Component {
                 <input type="text" value={title}
                     onChange={(event) => this.handleOnChangeTitle(event)}
                 />
-                <button type="button" className='add'>Add</button>
+                <button type="button" className='add'
+                    onClick={() => this.handleAddTodo()}
+                >
+                    Add
+                </button>
             </div>
         );
     }
