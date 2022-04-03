912030fcc445f4b8e8dd3dbf74536366bd70c14a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-03 20:52:05 +0900

    List to do

---
+++ src/views/Todos/ListTodo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListTodo extends Component {
+    render() {
+        return (
+            <div>
+                Hello List to do
+            </div>
+        );
+    }
+}
+
+export default ListTodo;
