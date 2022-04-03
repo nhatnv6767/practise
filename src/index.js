2960eeef46454ef5bd3bbbc82a5a95099cedf928
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-03 20:47:16 +0900

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as ReactDOMClient from 'react-dom/client'
+import App from './views/App';
 import reportWebVitals from './reportWebVitals';
+import './styles/global.scss';
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = ReactDOMClient.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
