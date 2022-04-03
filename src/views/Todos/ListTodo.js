85dc3349c6040da4ce5a2b9a1e292621062a52fa
nhatnv6767 <nhatnvse90183@gmail.com>
2022-04-03 20:58:22 +0900

    content

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 
 class ListTodo extends Component {
+
+    state = {
+        listTodos: [
+            { id: 'todo1', title: 'Doing homework' },
+            { id: 'todo2', title: 'Coding ReactJS' },
+            { id: 'todo3', title: 'Fixing bugs' },
+        ],
+    }
+
     render() {
+        let { listTodos } = this.state
         return (
             <div className="list-todo-container">
                 <div className="add-todo">
@@ -9,7 +19,15 @@ class ListTodo extends Component {
                     <button type="button" className='add'>Add</button>
                 </div>
                 <div className="list-todo-content">
-
+                    <div className="todo-child">
+                        Todo1
+                    </div>
+                    <div className="todo-child">
+                        Todo1
+                    </div>
+                    <div className="todo-child">
+                        Todo1
+                    </div>
                 </div>
             </div>
         );
